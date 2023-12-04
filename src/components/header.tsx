c96f26acb74e805820137bde6db7d1d699c2009d
Misho <chapidze.misho95@gmail.com>
2023-12-04 23:19:25 +0400

    s

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import { animated } from "@react-spring/web";
+
+const Header = () => {
+  return (
+    <animated.header>
+      <img src={} />
+    </animated.header>
+  );
+};
+
+export default Header;
